17da6f3d819d68353525f3e7ec73f6ebe3d4cb8a
macriidanu <macriidani@.gmail.com>
2024-10-17 20:05:18 +0300

    converted prices to eur, add a filter based on price, change price_old, price_new to price_now

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -1,7 +1,8 @@
 from lab1.src.consts import base_url
+from lab1.src.model.products_price_range import ProductsPriceRange
 from lab1.src.parser import parse_products
 from lab1.src.scraper import html_scraper
-
+from lab1.src.value_converter import convert_price
 
 
 def main():
@@ -10,8 +11,11 @@ def main():
     html = html_scraper(milk_url)
     products = parse_products(html)
 
-    for p in products:
-        print(p)
+    for product in products:
+        product.price_eur  = convert_price(product)
+
+    cheap_products = ProductsPriceRange(products, 0, 20)
+    print(cheap_products)
 
 
 
--- lab1/src/parser.py
+++ lab1/src/parser.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from lab1.src.consts import base_url
+from lab1.src.model.product import Product
 from lab1.src.scraper import html_scraper
 
 
@@ -20,23 +21,29 @@ def parse_products(html_content):
 
 
         if name_div.find("span", {"class": ["price-products-catalog-content__static"]}):
-            price_old = name_div.find("span", {"class": ["price-products-catalog-content__static"]}).get_text(strip=True)
-            price_new = ""
+            price_now = name_div.find("span", {"class": ["price-products-catalog-content__static"]}).get_text(strip=True)
+            price_old = ""
             discount = ""
         else:
             price_old = name_div.find("span", {"class": "price-products-catalog-content__old"}).get_text(strip=True)
-            price_new = name_div.find("span", {"class": "price-products-catalog-content__new"}).get_text(strip=True)
+            price_now = name_div.find("span", {"class": "price-products-catalog-content__new"}).get_text(strip=True)
             discount = name_div.find("div", {"class": "price-products-catalog-content__discount"}).get_text(strip=True)
 
-        products.append({
-                'name': p_name,
-                'price_old': price_old,
-                'price_new': price_new,
-                'discount': discount,
-                'link': p_link,
-                'category': p_category,
-                'subcategory': p_subcategory
-            })
+        product = Product(
+            name=p_name,
+            link=p_link,
+            price_old= price_old,
+            price_now=price_now,
+            discount=discount,
+            category=p_category,
+            sub_category=p_subcategory
+        )
+        try:
+            product.validate()
+            products.append(product)
+        except Exception as e:
+            print(e)
+
 
     return products
 
