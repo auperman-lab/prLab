ce8bfba9c70bebc56a1ca73d931f8c23ed1a754a
macriidanu <macriidani@.gmail.com>
2024-12-07 19:05:44 +0200

    started scraping from images

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -4,20 +4,19 @@ from lab1.src.scraper import http_scraper
 
 
 def main():
-    milk_path = "/ro/catalog/produse_lactate?page=1"
-    milk_url = "https://" + base_url + milk_path
-    categories_path = "/ro/catalog"
-    categories_url = "https://"+base_url+ categories_path
-
-    html = http_scraper(categories_url)
-
-    parse_all_categories(html)
-
-    # html = tcp_scraper(milk_path)
+    # milk_path = "/ro/catalog/produse_lactate?page=1"
+    # milk_url = "https://" + base_url + milk_path
+    # categories_path = "/ro/catalog"
+    # categories_url = "https://"+base_url+ categories_path
     #
+    # html = http_scraper(categories_url)
     #
-    # products = parse_products(html)
-    #
+    # parse_all_categories(html)
+
+    pass
+
+
+
 
 
 
